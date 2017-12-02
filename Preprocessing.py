c6572f1aa55814c15ea4cccffb1168490f809a63
Simo <a>
2017-12-02 19:13:27 +0200

    data exploring

--- Preprocessing.py
+++ Preprocessing.py
@@ -6,9 +6,27 @@ from sklearn.metrics import mean_squared_error
 import numpy as np
 import matplotlib.pyplot as plt
 import lightgbm as lgb
+import matplotlib.pyplot as plt
+plt.style.use(style='ggplot')
+plt.rcParams['figure.figsize'] = (10, 6)
 
 data = pd.read_csv("C:\\Users\\Simo\\Desktop\\HousePrices\\train.csv")
 data.drop('Id', axis=1, inplace=True)
+
 categoricals = data.select_dtypes(exclude=[np.number])
-data = pd.get_dummies(data,columns=categoricals,dummy_na=True)
-data= data.fillna(0)
+numericals = data.select_dtypes(include=[np.number])
+# data = data.fillna(0)
+
+for numeric in numericals:
+    print("###############################################")
+    print(numeric)
+    print("Number of missing values: "+ str(data[numeric].isnull().sum()))
+    print(data[numeric].value_counts())
+    data[numeric] = data[numeric].fillna(0)
+    print("skeweness : "+str(data[numeric].skew()))
+    print(data[numeric].describe())
+    plt.hist(data[numeric], color='blue')
+    plt.show()
+
+
+# data = pd.get_dummies(data,columns=categoricals,dummy_na=True)

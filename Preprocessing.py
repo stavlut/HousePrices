7f45e7ee4ca5e25b79083688aba487c6c3351a26
Simo <a>
2017-12-04 14:12:08 +0200

    data exploring

--- Preprocessing.py
+++ Preprocessing.py
@@ -14,19 +14,49 @@ data = pd.read_csv("C:\\Users\\Simo\\Desktop\\HousePrices\\train.csv")
 data.drop('Id', axis=1, inplace=True)
 
 categoricals = data.select_dtypes(exclude=[np.number])
-numericals = data.select_dtypes(include=[np.number])
-# data = data.fillna(0)
+def numericals_data_preprocessing(data):
+    numericals = data.select_dtypes(include=[np.number])
+    # MSSubClass - make this feature categorial
+    data["MSSubClass"] = pd.Categorical(data["MSSubClass"])
+    numericals = numericals.drop("MSSubClass",axis=1)
+    # fill na with mean
+    data['LotFrontage'].fillna(data['LotFrontage'].mean(),inplace=True)
+    # fill na with 0
+    data['MasVnrArea'].fillna(0,inplace=True)
 
-for numeric in numericals:
-    print("###############################################")
-    print(numeric)
-    print("Number of missing values: "+ str(data[numeric].isnull().sum()))
-    print(data[numeric].value_counts())
-    data[numeric] = data[numeric].fillna(0)
-    print("skeweness : "+str(data[numeric].skew()))
-    print(data[numeric].describe())
-    plt.hist(data[numeric], color='blue')
-    plt.show()
+    # clean outliers
+    data= data[data['LotFrontage'] <= 200]
+    data= data[data['LotArea'] <= 60000]
+    data= data[data['BsmtFinSF1'] <= 2500]
+    data= data[data['GrLivArea'] <= 4000]
+    for numeric in numericals:
+        if(data[numeric].skew()>0.75):
+            data[numeric] = np.log1p(data[numeric])
+
+
+    # remove features
+    data.drop('BsmtFinSF2',axis=1, inplace=True)
+    data.drop('LowQualFinSF',axis=1, inplace=True)
+    data.drop('3SsnPorch',axis=1, inplace=True)
+    data.drop('ScreenPorch',axis=1, inplace=True)
+    data.drop('PoolArea',axis=1, inplace=True)
+    data.drop('MiscVal',axis=1, inplace=True)
+
+    return data
+
+# print(data['LotArea'].isnull().sum())
+
+
+# for numeric in numericals:
+#     print("###############################################")
+#     print(numeric)
+#     print("Number of missing values: "+ str(data[numeric].isnull().sum()))
+#     print(data[numeric].value_counts())
+#     data[numeric] = data[numeric].fillna(0)
+#     print("skeweness : "+str(data[numeric].skew()))
+#     print(data[numeric].describe())
+#     plt.hist(data[numeric], color='blue')
+#     plt.show()
 
 
 # data = pd.get_dummies(data,columns=categoricals,dummy_na=True)
--- main.py
+++ main.py
@@ -6,19 +6,16 @@ from sklearn.metrics import mean_squared_error
 import numpy as np
 import matplotlib.pyplot as plt
 import lightgbm as lgb
+import Preprocessing
 
-plt.style.use(style='ggplot')
-plt.rcParams['figure.figsize'] = (10, 6)
-
-
-
-
+data = pd.read_csv("C:\\Users\\Simo\\Desktop\\HousePrices\\train.csv")
+data.drop('Id', axis=1, inplace=True)
+data = Preprocessing.numericals_data_preprocessing(data)
+categoricals = data.select_dtypes(exclude=[np.number])
+# numericals = data.select_dtypes(include=[np.number])
+data = pd.get_dummies(data,columns=categoricals,dummy_na=True)
+data = data.fillna(0)
 train, test = train_test_split(data, test_size=0.2,random_state=1)
-print(train.SalePrice.describe())
-
-print(train.SalePrice.skew())
-plt.hist(train.SalePrice, color='blue')
-plt.show()
 
 y_truth = test["SalePrice"]
 test = test.drop('SalePrice', axis=1)
@@ -34,7 +31,7 @@ params = {
     'objective': 'regression',
     'metric': {'l2', 'auc'},
     'num_leaves': 31,
-    'learning_rate': 0.05,
+    'learning_rate': 0.01,
     'feature_fraction': 0.9,
     'bagging_fraction': 0.8,
     'bagging_freq': 5,
@@ -58,7 +55,7 @@ y_predict = rfr.predict(test)
 
 ######## sklearn gbm  ################
 
-params2 = {'n_estimators': 500, 'max_depth': 4, 'min_samples_split': 2,
+params2 = {'n_estimators': 7200, 'max_depth': 4, 'min_samples_split': 2,
           'learning_rate': 0.01, 'loss': 'ls'}
 clf = GradientBoostingRegressor(**params2)
 
